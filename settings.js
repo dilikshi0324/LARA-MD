c6c9400c849d01dee44971feae509671ad2a6f0b
dilikshi0324 <139313818+dilikshi0324@users.noreply.github.com>
2025-02-18 22:51:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "uzgUjSjB#Rgp6VwxJ4ul5w9eK9gnbkJBt1euCnwdZQ9nKAP1koOA", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 STATUES_REPLY: process.env.STATUES_REPLY || "true",
